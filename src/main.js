6abbb2f711e368c4fb434f1a1683628cac5f26df
seale <1801157108@qq.com>
2020-10-11 00:27:35 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/*引入通用样式*/
+import '@/assets/css/sup.core.css'
+
+
 Vue.config.productionTip = false
 
 new Vue({
